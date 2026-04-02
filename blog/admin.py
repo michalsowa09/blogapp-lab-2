4b9f66dec3e72cc0f7a1acb446994faf530291d3
Michał Sowa <michalsowa9@o2.pl>
2026-04-02 11:13:58 +0200

    Initialize database and advanced administrative access

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from models import Post
 
-# Register your models here.
+@admin.register(Post) #tu rejestruję model Post w panelu admina
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "status", "published_at")
+    #widoczne kolumny na liście postów - admin
+
+    list_filter = ("status", "published_at", "author")
+    #filtry
+
+    search_fields = ("title", "content")
+    #pole wyszukiwania
+
+    date_hierarchy = "published_at"
+    #nawigacja po dacie
+
