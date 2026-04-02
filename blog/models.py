d8b426e60b749d72f90e9d1766e5d3a4d8a96849
Michał Sowa <michalsowa9@o2.pl>
2026-04-02 10:38:17 +0200

    Define Post model with status and ordering

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):  #definicja modelu (czyli tabeli w bazie danych)
+    STATUS_CHOICES = (
+        ('draft', 'Szkic'), #wersja robocza posta
+        ('published', 'Opublikowano'), #opublikowany post
+    )
+
+    title = models.CharField(max_length=200, verbose_name="Tytuł")  #krótki tekst (max 200 znaków)
+    content = models.TextField(verbose_name="Treść") #dłuższy tekst (bez limitu znaków)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor") #powiązanie z użytkownikiem, CASCADE = jak usunie się użytkownika, jego posty też znikną
+    created_at = models.DateTimeField(auto_now_add=True) #data utworzenia (ustawiana automatycznie tylko raz)
+    published_at = models.DateTimeField(default=timezone.now, verbose_name="Data publikacji")   #data publikacji (domyślnie teraz)
+    status = models.CharField(max_length=10, choices = STATUS_CHOICES, default='published') #status posta (draft lub published)
+
+    class Meta:
+        ordering = ["-published_at"] #Najnowsze na górze
+        verbose_name = "Post"
+        verbose_name_plural = "Posty"
+
+    def __str__(self):
+        return self.title
